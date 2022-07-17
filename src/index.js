75f69e337c3d4bcea5162e8c94e20b07f70313ca
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-17 12:56:20 -0300

    feat: return status and error message to non-existent user

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ function checksExistsUserAccount(request, response, next) {
 
   const user = users.find(user => user.username === username)
 
+  if (!user) {
+    return response.status(404).json({ error: 'User not found'})
+  }
+  
   request.user = user
 
   return next()
